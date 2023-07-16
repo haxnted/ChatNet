e5679e5742d008008b2298c2c0317397194b2fdf
haxnted <bagnencko.sasha1@gmail.com>
2023-07-16 13:14:11 +0300

    window layout design

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Drawing;
 namespace ChatNet
 {
     /// <summary>
@@ -23,6 +23,57 @@ namespace ChatNet
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void SendMessage(object sender, RoutedEventArgs e)
+        {
+
+
+
+
+
+
+
+
+
+
+
+
+            //int length = TextForMessage.Text.Length;
+            //if (string.IsNullOrEmpty(TextForMessage.Text))
+            //{
+            //    return;
+            //}
+            //int width = TextForMessage.Text.Length;
+            //for (int i = 0; i < length; i++)
+            //{
+            //    width += 10;
+            //}
+            //Label textLabel = new Label()
+            //{
+            //    HorizontalAlignment = HorizontalAlignment.Left,
+            //    VerticalAlignment = VerticalAlignment.Center,
+            //    Margin = new Thickness(5),
+            //    Width = width, 
+            //    Height = 30,
+            //    FontSize = 10.0,
+            //    Background = Brushes.LightGreen,
+            //    Content = TextForMessage.Text,
+            //};
+            //Paragraph paragraph = new Paragraph()
+            //{
+            //    LineHeight = 0.1,
+            //    BorderThickness = new Thickness(0),
+            //    TextAlignment = TextAlignment.Right
+            //};
+            //InlineUIContainer container = new InlineUIContainer(textLabel);
+            //paragraph.Inlines.Add(container);
+            //paragraph.Inlines.Add(new LineBreak());
+
+            //FlowDocument flow = Messages.Document;
+            //flow.Blocks.Add(paragraph);
+
+            //TextForMessage.Text = string.Empty;
         }
     }
 }
